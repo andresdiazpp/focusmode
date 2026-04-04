3de62a87e5a1404b3a00a49ebd6f3c6534af1cfa
Andres Diaz <andres.diaz.paez.07@gmail.com>
2026-04-04 09:38:10 -0500

    Initial Commit

---
+++ FocusMode/FocusModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusModeApp.swift
+//  FocusMode
+//
+//  Created by Andres Diaz on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
